9d08f8eda9547b3823dc0246da41df6c3452f7cf
laraib_sidd <laraib.sidd7@gmail.com>
2023-12-09 18:44:46 +0530

    code format

--- lib/bot.py
+++ lib/bot.py
@@ -5,8 +5,9 @@ from techencalve_extract import extract_techenclave_data
 import os
 
 # Define your constants
-TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
-TELEGRAM_CHAT_ID =  os.environ.get('TELEGRAM_CHAT_ID')
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
+
 
 def main():
     # Initialize the Telegram Bot
@@ -21,12 +22,13 @@ def main():
         for post in posts:
             send_post_to_telegram(bot, post, TELEGRAM_CHAT_ID)
 
+
 def send_post_to_telegram(bot, post, chat_id):
-    subreddit = post.get('subreddit', 'Techenclave')
-    title = post.get('title', 'Null')
-    posted_ago = post.get('posted_ago', 'Null')
-    url = post.get('url', 'Null')
-    text = post.get('selftext', 'Null')
+    subreddit = post.get("subreddit", "Techenclave")
+    title = post.get("title", "Null")
+    posted_ago = post.get("posted_ago", "Null")
+    url = post.get("url", "Null")
+    text = post.get("selftext", "Null")
 
     msg = f"""
         Subreddit: {subreddit}
@@ -35,8 +37,9 @@ def send_post_to_telegram(bot, post, chat_id):
         URL: {url}
         Text: {text}
     """
-    
+
     bot.send_message(chat_id=chat_id, text=msg)
+
 
 if __name__ == "__main__":
     main()
--- lib/techencalve_extract.py
+++ lib/techencalve_extract.py
@@ -15,34 +15,40 @@ def calculate_time_difference(date_string):
     minutes_passed = total_seconds_passed / 60
 
     time_unit = "hours" if minutes_passed >= 60 else "minutes"
-    formatted_time = f"{minutes_passed:.2f}" if time_unit == "minutes" else f"{minutes_passed / 60:.2f}"
-    
+    formatted_time = (
+        f"{minutes_passed:.2f}"
+        if time_unit == "minutes"
+        else f"{minutes_passed / 60:.2f}"
+    )
+
     return f"{formatted_time} {time_unit} have passed. This post was created {ist_dt:%Y/%m/%d-%H:%M} hours"
+
 
 def convert_encoded_content_to_text(encoded_content):
     parsed_html = BeautifulSoup(encoded_content, features="html.parser")
     return parsed_html.get_text()
 
 
-
 def extract_techenclave_data():
     dict_data = xmltodict.parse(requests.get(os.getenv("TECHENCLAVE_URL")).content)
     mongo_client = create_mongo_client()
-    mongo_collection = mongo_client['techencalve']
+    mongo_collection = mongo_client["techencalve"]
     techenclave_data = []
 
     for item in dict_data.get("rss", {}).get("channel", {}).get("item", []):
-        id_exist = check_post_id_mongo(data['guid']['#text'], mongo_collection)
+        id_exist = check_post_id_mongo(data["guid"]["#text"], mongo_collection)
         if id_exist:
             continue
-        else:  
+        else:
             techenclave_dict = {
                 "title": item.get("title", ""),
                 "url": item.get("link", ""),
                 "posted_ago": calculate_time_difference(item.get("pubDate", "")),
-                "selftext": convert_encoded_content_to_text(item.get("content:encoded", "")),
+                "selftext": convert_encoded_content_to_text(
+                    item.get("content:encoded", "")
+                ),
             }
-            insert_mongo_collection(data['guid']['#text'],mongo_collection)
+            insert_mongo_collection(data["guid"]["#text"], mongo_collection)
             techenclave_data.append(techenclave_dict)
     return techenclave_data
 
@@ -50,4 +56,3 @@ def extract_techenclave_data():
 if __name__ == "__main__":
     data = extract_techenclave_data()
     data = data[0]
-   
